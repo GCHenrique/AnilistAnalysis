456af74ccbd8cebbf04b877624afd383f70b041a
Henrique Garcia Costa <henriquegarciacosta1@gmail.com>
2026-08-24 20:27:57 -0300

    Add error handling at main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from anilist_analysis import pipeline
+from anilist_analysis.api import AnilistAPIError
 
 
 def main():
-    pipeline.run_comparison_2()
+    try:
+        pipeline.run_comparison_2()
+    except AnilistAPIError as e:
+        print(e)
 
 
 if __name__ == "__main__":
